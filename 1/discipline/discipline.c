b8435d6627baf669112f1bae7a68b0c4fe776efe
gabreudev <biel0alves0@gmail.com>
2024-09-13 13:55:32 -0300

    Another solucion

--- 1/discipline/discipline.c
+++ 1/discipline/discipline.c
@@ -63,74 +63,150 @@ int get_code()
     return discipline_code++;
 }
 
-Discipline *insert_discipline(Discipline *root, Discipline *new_subject) 
-{
-    if(root == NULL)
-        return root;
+Discipline* insert_discipline(Discipline *root, Discipline *new_subject) {
+    Discipline *result = root;
 
-    // Compara o código da disciplina para determinar a posição
-    if (strcmp(root->discipline_name, new_subject->discipline_name) == 0) 
-    {
+    if (root == NULL) {
+        result = new_subject;
+    } else if (strcmp(root->discipline_name, new_subject->discipline_name) == 0) {
+        
         printf("Discipline with name %s already exists!\n", root->discipline_name);
-        return root;
-    }
-
-    // Inserção recursiva na subárvore da esquerda ou direita
-    if (new_subject->discipline_code < root->discipline_code) 
+        
+    } else if (new_subject->discipline_code < root->discipline_code) {
+        
         root->left = insert_discipline(root->left, new_subject);
-    else if (new_subject->discipline_code > root->discipline_code) 
-        root->right = insert_discipline(root->right, new_subject);
+    } else if (new_subject->discipline_code > root->discipline_code) {
 
-    return root;
+        root->right = insert_discipline(root->right, new_subject);
+    }
+    
+    return result;
 }
 
+
+// void register_discipline(Discipline **root, Course *course)
+// {
+//     int temp = 0;
+
+//     // Aloca memória para um novo curso
+//     Discipline *new = allocate_discipline();
+//     check_allocation(new, "register discipline");
+    
+//     printf("Enter discipline name: ");
+//     setbuf(stdin, NULL);
+//     scanf("%[^\n]", new->discipline_name);
+
+//     new->discipline_code = GET_DISCIPLINE_CODE();
+
+//     do
+//     {
+//         printf("---------- WORKLOAD ----------\n1 - 30 hours\n2 - 60 hours\n3 - 90 hours\n");
+//         scanf("%d", &temp);
+
+//         switch(temp)
+//         {
+//         case 1:  new->workload = HOURS_30; break;
+//         case 2:  new->workload = HOURS_60; break;
+//         case 3:  new->workload = HOURS_90; break;
+//         default: break;
+//         }
+//     } 
+//     while(!valid_answer(1, 3, temp));
+
+//     do {
+//         printf("The course %s has %d periods, where the %s discipline will be placed\n",
+//         course->course_name, course->num_periods, new->discipline_name);
+//         scanf("%d", &temp);
+//     } 
+//     while(!valid_answer(1, course->num_periods, temp));
+//     new->period = temp;
+
+
+//     // Insere o curso na árvore de cursos 
+//     // TODO: make a better solution
+//     if(root == NULL)
+//         insert_discipline(*root, new);
+//     else
+//         *root = new;
+    
+//     printf("Discipline successfully registered!\n");
+// }
+
+// Another solucion
 void register_discipline(Discipline **root, Course *course)
 {
     int temp = 0;
+    int exists = 0; 
+    char discipline_name[100];
 
-    // Aloca memória para um novo curso
-    Discipline *new = allocate_discipline();
-    check_allocation(new, "register discipline");
-    
+    // Pega o nome da disciplina para comparar
     printf("Enter discipline name: ");
     setbuf(stdin, NULL);
-    scanf("%[^\n]", new->discipline_name);
+    scanf("%[^\n]", discipline_name);
 
-    new->discipline_code = GET_DISCIPLINE_CODE();
+    // Verifica se a disciplina já existe na árvore
+    if (find_discipline(*root, discipline_name)) {
+        printf("Discipline with name %s already exists!\n", discipline_name);
+        exists = 1; // Marca que a disciplina já existe
+    }
 
-    do
-    {
-        printf("---------- WORKLOAD ----------\n1 - 30 hours\n2 - 60 hours\n3 - 90 hours\n");
-        scanf("%d", &temp);
+    if (!exists) {
 
-        switch(temp)
+        Discipline *new = allocate_discipline();
+        check_allocation(new, "register discipline");
+
+        strcpy(new->discipline_name, discipline_name);
+        new->discipline_code = GET_DISCIPLINE_CODE();
+
+        do
         {
-        case 1:  new->workload = HOURS_30; break;
-        case 2:  new->workload = HOURS_60; break;
-        case 3:  new->workload = HOURS_90; break;
-        default: break;
-        }
-    } 
-    while(!valid_answer(1, 3, temp));
+            printf("---------- WORKLOAD ----------\n1 - 30 hours\n2 - 60 hours\n3 - 90 hours\n");
+            scanf("%d", &temp);
 
-    do {
-        printf("The course %s has %d periods, where the %s discipline will be placed\n",
-        course->course_name, course->num_periods, new->discipline_name);
-        scanf("%d", &temp);
-    } 
-    while(!valid_answer(1, course->num_periods, temp));
-    new->period = temp;
+            switch(temp)
+            {
+                case 1:  new->workload = HOURS_30; break;
+                case 2:  new->workload = HOURS_60; break;
+                case 3:  new->workload = HOURS_90; break;
+                default: break;
+            }
+        } 
+        while(!valid_answer(1, 3, temp));
 
+        do {
+            printf("The course %s has %d periods, where the %s discipline will be placed\n",
+            course->course_name, course->num_periods, new->discipline_name);
+            scanf("%d", &temp);
+        } 
+        while(!valid_answer(1, course->num_periods, temp));
+        new->period = temp;
 
-    // Insere o curso na árvore de cursos 
-    // TODO: make a better solution
-    if(root == NULL)
-        insert_discipline(*root, new);
-    else
-        *root = new;
-    
-    printf("Discipline successfully registered!\n");
+        if (*root == NULL)
+            *root = new;
+        else
+            insert_discipline(*root, new);
+
+        printf("Discipline successfully registered!\n");
+    }
 }
+Discipline* find_discipline(Discipline *root, const char *name) {
+    Discipline *result = NULL;
+
+    while (root != NULL) {
+        int cmp = strcmp(name, root->discipline_name);
+        if (cmp == 0) {
+            result = root; // Encontrou a disciplina
+            root = NULL; // Para sair do loop
+        } else if (cmp < 0) {
+            root = root->left; 
+        } else {
+            root = root->right; 
+        }
+    }
+
+    return result;
+}
+
 
 void remove_subject(Course *course, int discipline_code)
 {
